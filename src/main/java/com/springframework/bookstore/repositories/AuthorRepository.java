6cf36f06b67ada3261d6e1a751b696fba4413128
Gilbert Ngeno <ngenogilbert07@gmail.com>
2022-03-14 11:31:35 +0300

    created models, controllers

---
+++ src/main/java/com/springframework/bookstore/repositories/AuthorRepository.java
@@ -0,0 +1,8 @@
+package com.springframework.bookstore.repositories;
+
+
+import com.springframework.bookstore.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+}
